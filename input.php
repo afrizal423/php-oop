17b1d48c34902b578a4d50959693d86f208b8819
afrizal423 <afrizal2499@gmail.com>
2018-05-10 19:37:38 +0700

    v1

--- input.php
+++ input.php
@@ -1,10 +1,7 @@
 <html>
 <head>
 	<title>PHP OOP</title>
-	<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
 </head>
 <body>
